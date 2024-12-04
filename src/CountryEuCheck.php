478b5dbf8b1f747fe7601e69a1b252c81faec6b3
DonCamillo11 <moin@maiocchi.de>
2024-12-04 13:06:44 +0100

    changed 'checkEuMembership' method to static

--- src/CountryEuCheck.php
+++ src/CountryEuCheck.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\File;
 
 class CountryEuCheck
 {
-    public function checkEuMembership($country) : bool
+    public static function checkEuMembership($country) : bool
     {
         $euCountries = File::json(dirname(__FILE__).'/../data/eu-country-list.json');
         if(array_key_exists($country, $euCountries) || in_array($country, $euCountries)) {
